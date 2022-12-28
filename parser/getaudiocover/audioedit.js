c3ed830a7fb7210e4a2695f56e0cf3b31f312569
m0onpi <michael.pigottbiz@gmail.com>
2022-12-28 14:36:32 +0000

    Main

---
+++ parser/getaudiocover/audioedit.js
@@ -0,0 +1,22 @@
+const ffmpeg = require("fluent-ffmpeg")
+const execSync = require('child_process').execSync;
+var info = require('/projectsjs/video_auto/src/obj.json')
+
+ffmpeg({source:'/projectsjs/parser/audioeditor/audio.mp3'})
+.setStartTime(info[1])
+.duration(21)
+.on('start',function(commandLine){
+    console.log("Trimming")
+})
+.on('error', function(err){
+    console.log("Song not downloadable", + err)
+})
+.on("end", function(err){
+    console.log("Trimmed")
+    execSync('npm start --prefix video_auto')
+
+    
+    
+})
+.saveToFile("/projectsjs/video_auto/public/editaudio.mp3")
+
---
+++ parser/getaudiocover/getstuff.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const execSync = require('child_process').execSync;
+var bodyParser = require('body-parser')
+const app = express();
+fs = require("fs"); 
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var obj = []   
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+    
+app.post('/', urlencodedParser, (req, res) => {
+    let songurl = req.body.songurl
+    let startime = req.body.starttime
+    obj.push(songurl, startime) 
+    let object = JSON.stringify(obj)
+    console.log(obj)  
+    fs.writeFile('./video_auto/src/obj.json', object, err => {
+      if (err) {
+          console.log('Error writing file', err)
+      } else {
+      console.log('Successfully wrote file')
+
+      
+      }})  
+    res.sendFile(__dirname + '/index.html');
+});
+app.listen(8800)
+
+const SoundCloud = require("soundcloud-scraper");
+const client = new SoundCloud.Client();                                                
+Stream = require('stream').Transform, 
+fs = require("fs");
+var http = require('http')
+var https = require('https')
+var info = require('/projectsjs/video_auto/src/obj.json')
+
+client.getSongInfo(info[0])
+.then(async song => {
+    const stream = await song.downloadProgressive();
+    const writer = stream.pipe(fs.createWriteStream(`/projectsjs/parser/audioeditor/audio.mp3`));
+
+    https.request(song.thumbnail, function(response) {                                        
+      var data = new Stream();                                                      
+      response.on('data', function(chunk) {                                       
+        data.push(chunk);     
+                                                            
+      });
+
+      response.on('end', function() {  
+        fs.writeFileSync('/projectsjs/video_auto/public/image.png', data.read());
+        require('child_process').fork('/projectsjs/parser/audioeditor/blur.js')
+        require('child_process').fork('/projectsjs/parser/getaudiocover/title.js')
+        require('child_process').fork('/projectsjs/parser/getaudiocover/audioedit.js')
+        
+        
+        
+        
+      });
+    }).end()
+    
+    
+  });
+
+
