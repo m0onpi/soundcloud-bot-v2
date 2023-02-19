0ac7e501acd11501dfca8d814f4e04e177c0805c
m0onpi <michael.pigottbiz@gmail.com>
2023-02-19 00:02:38 +0000

    change get

--- main/delete.js
+++ main/delete.js
@@ -1,22 +1,12 @@
 const {getFirestore, doc, deleteDoc} = require("firebase/firestore");
 const {initializeApp} = require('firebase/app');
-
+const {firebaseConfig} = require('./config')
 const ID = require('./requests.json')
 const documentID = ID[0]._DOCUMENT_ID_
 const initializeFB = async () => {
     // USE YOUR FIREBASE PROJECT SETTINGS:
     //   Firebase Console >> Project Overview >> Apps
     //    >> (select or "Add App" web app) >> Config
-    const firebaseConfig = {
-      apiKey: "AIzaSyDCH9Uy7Ne_ZRk2B0Gy-E3ZKZG6Oz0BYTc",
-      authDomain: "dntmedia-v2.firebaseapp.com",
-      projectId: "dntmedia-v2",
-      storageBucket: "dntmedia-v2.appspot.com",
-      messagingSenderId: "333934549913",
-      appId: "1:333934549913:web:7da868584c114a153b5108",
-      measurementId: "G-2RPL6H6P2R"
-    };
-  
     let fbApp = initializeApp(firebaseConfig);
     FIRESTORE = getFirestore(fbApp);
 }
